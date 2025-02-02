3619a3b43302d1334fefd49ca41c70a92320736c
Jesse Otim <otimdanieljesse@gmail.com>
2025-02-02 12:19:56 +0300

    Refactor code

--- app/connection.js
+++ app/connection.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 //mongoDB connection string
 const url = process.env.DB_URL;
 
-mongoose.connect(url,{
+mongoose
+  .connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(()=>{
-  console.log('database connected!');})
-.catch(err => console.log(err));
+  })
+
+  .then(() => {
+    console.log("database connected!");
+  })
+  .catch((err) => console.log(err));
